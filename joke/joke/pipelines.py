ade6f8b058989ee1f772a7826c53c539a18aa62e
xxhuang <tiancaihxx@hotmail.com>
2016-04-06 00:33:52 -0400

    add a simple frontend for to search jokes

--- joke/joke/pipelines.py
+++ joke/joke/pipelines.py
@@ -6,13 +6,20 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
+from pymongo import MongoClient
 
 class JokePipeline(object):
     def __init__(self):
         self.file = open('joke.dat', 'wb')
+        self.client = MongoClient()
     def process_item(self, item, spider):
         #val = "\t{0}t\{1}\n".format(item['title'], item['content'])
         #self.file.write( val )
         line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        db = self.client.joke
+        jks = db.joke_table
+        insert_data_tmp = {"content":item['content'], "title":item['title'],
+            "state":"new"}
+        jks.insert(insert_data_tmp)
         self.file.write(line)
         return item
